c7aa0eb35911f57feace61fda60962f71771748f
M1roel <ppfautsch@yahoo.de>
2024-12-04 12:30:10 +0000

    first component - landingpage

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule],
+    template: '<h1>Hello World</h1>',
+    styleUrls: ['./landingPage.component.scss']
+})
+
+export class LandingPageComponent {
+
+}
